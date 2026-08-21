30d661da5c28e5e6c2bddde299da0a7e93319cd7
202507040 <sun31410@gmail.com>
2026-08-21 12:28:41 +0900

    아이콘 넣는코드작성

--- MainGuestbook/MainGuestbook/InitUI.cpp
+++ MainGuestbook/MainGuestbook/InitUI.cpp
@@ -14,41 +14,43 @@ void InitUI(HWND hWnd, HINSTANCE  hInst)
   HMENU hFileMenu = CreatePopupMenu();
 
 
-  AppendMenu(hFileMenu, MF_STRING, 1001, L"저장하기"); // 1001 사건번호 
+  AppendMenu(hFileMenu, MF_STRING, 1001, L"저장하기"); 
   AppendMenu(hFileMenu, MF_STRING, 1002, L"불러오기");
   AppendMenu(hFileMenu, MF_STRING, 1005, L"상욱이");
-  AppendMenu(hFileMenu, MF_SEPARATOR, 0, nullptr); // 가로 구분선 긋기
+  AppendMenu(hFileMenu, MF_SEPARATOR, 0, nullptr);
   AppendMenu(hFileMenu, MF_STRING, 1003, L"끝내기");
 
-  // 다 채운 [파일] 드롭다운 메뉴를 맨 위 큰 메뉴바에 이름표 달아서 붙이기
   AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hFileMenu, L"파일");
-
-
-
 
   // 세팅 메뉴를 위한 코드들
   HMENU hSetting = CreatePopupMenu();
+
   AppendMenu(hSetting, MF_STRING, 2001, L"1");
   AppendMenu(hSetting, MF_STRING, 2002, L"2");
   AppendMenu(hSetting, MF_STRING, 2003, L"3");
 
-
   AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hSetting, L"세팅");
-
 
   SetMenu(hWnd, hMenuBar); // 바를 화면인데에 붙히는 함수
 
 
   HWND hToolBar = CreateWindowW(L"STATIC", L"",
-    WS_CHILD | WS_VISIBLE, // 테두리가 있는 기본 스타일
+    WS_CHILD | WS_VISIBLE,
     0, 0,
-    2000, 50,
+    2000, 70,
     hWnd, nullptr, hInst, nullptr);
 
-  // 부모 창(8번째 인자)을 hWnd가 아니라 hToolBar로 지정하면 바 위에 버튼이 찰싹 붙습니다.
-  CreateWindowW(L"BUTTON", L"선택", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-    15, 10, 60, 30, // 바(hToolBar) 안에서의 위치
+  // 첫번째 버튼!
+  HWND Button = CreateWindowW(L"BUTTON", L"선택", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_ICON ,
+    15, 10, 50, 50, 
     hWnd, (HMENU)3001, hInst, nullptr);
+
+
+
+  // 아이콘 파일명만 주면 바로 그 파일안에서 어려 정보를 불러오는 함수이다 ex 아이콘,비트맵그림,마우스커서그림 
+  HICON hicon = (HICON)LoadImageW(nullptr, L"images/spray.ico", IMAGE_ICON,50, 50,LR_LOADFROMFILE );
+
+  SendMessage(Button, BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)hicon);
 
 
 }
