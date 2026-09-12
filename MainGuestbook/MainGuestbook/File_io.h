c3e322cb442b1707e52793ce5ab2e3b76011ab88
jhc0410 <a41655654@gmail.com>
2026-09-12 20:42:22 +0900

    파일 저장,불러오기 2차 저장

--- MainGuestbook/MainGuestbook/File_io.h
+++ MainGuestbook/MainGuestbook/File_io.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include "StructData.h"
 
-bool ShowFileDialog(HWND hWnd, wchar_t* filePath, bool isSave);
-bool File_Save(const wchar_t* path, std::vector<Line>& lines);
-bool File_Call(const wchar_t* path, std::vector<Line>& lines);
+bool ShowFileDialog(HWND hWnd, wchar_t* filePath, bool isSave); //파일 보여주기
+bool File_Save(const wchar_t* path, std::vector<Line>& lines); //파일저장
+bool File_Call(const wchar_t* path, std::vector<Line>& lines); // 파일불러오기
+bool File_AutoSave(std::vector<Line>& lines); //파일 자동 저장
